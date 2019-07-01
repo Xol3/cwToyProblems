a83d58beecd618a994743b0cf303a80f6bfbec6b
thinkpad <xoalxiyetl@gmail.com>
2019-07-01 14:22:02 -0500

    learning js

---
+++ LearningJS/js1.js
@@ -0,0 +1,81 @@
+
+Dev Tools y La Consola ************************
+1.  Abre developers tools; Cuales son las tabs que aparecen y que informacion ensenan?
+
+2.  Haz un Alert que diga el nombre de tu mascota
+
+3.  Que es el proposito de console.log?
+
+4.  Como puedes dejar una linea completa de espacio en la consola?
+      
+5.  Crea un commentario usando las dos differentes formas. 
+
+6.  document.getElementsByTagName("h1")[0].style.color = "#ff073a";
+      que paso cuando corres este commando en la consola? 
+
+7. javascript:var imgs = document.getElementsByTagName("img");for(var i=0, l=imgs.length;i<l;i++){imgs[i].src = "https://img.memecdn.com/Chabelo-is-here_o_39696.jpg";}     
+      que paso cuando corres este commando en la consola? 
+
+
+Variables ************************************
+
+1. Crea las siguientes variables con los siguientes nombres apuntando a un numero; transportacion, comida, renta.
+
+2. En la consola agrega los 3 variables.
+
+3. Crea 3 variable apuntando al los nombres de tus familiares.
+
+4. Crea un alerta con que despliege los 3 nombres. 
+
+
+Typos de Datos  ******************************
+  Primitivos
+    -Boolean 
+    -Number
+    -String
+  Empty  
+    -Null
+    -Undefined
+  Object
+    -Array
+    -Function  
+
+Numbers
+2 + 10 - 19 
+
+
+-20 + -19 - (-10) 
+
+
+(10/5) * 4 - 20
+
+
+4096 % 12 
+
+      
+
+Strings
+
+
+1. Haz un console.log que despliege "se llama "pancho" "
+
+
+Boolean
+
+1. Crea dos variables; 
+uno apuntando a false y el otro a true.
+
+
+Null, Undefined, and NaN
+
+1. Crea un variable sin apuntar a ningin tipo de dato, cual es el resultado si haces un console.log()?
+
+
+Extra Credit 
+
+LUNNNCH! => conde 10 , tepito 5 ,  MH 3
+  Crea una variable llamada CUENTA cual sea la suma de la siguiente orden: 3 tacos: 63 pesos, 1 gringa: 24 pesos , 2 chescos:  30 pesos.
+  Crea una variable llamada Cartera que equivalga a  520 pesos.
+  Al llegar a tu nave vez que el poli le puso la arana, y despues de la mordida solo te quedan 50 pesos. Crea una variable que haga un console.log donde imprima la referencia al resultado de la Mordida. 
+
+index! => Haz un console.log que solo despliege la "o" en "pancho"?
